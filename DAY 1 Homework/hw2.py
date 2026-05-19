1a5b59e8ced78515526d33a52f744c423ce2bcc2
crazylad-ctrl <ujjwalsubedi603@gmail.com>
2026-05-19 22:14:34 +0545

    Rename folder-name to DAY 1 Homework

---
+++ DAY 1 Homework/hw2.py
@@ -0,0 +1,12 @@
+#  sep
+print("Monday", "Tuesday", "Wednesday", sep="-")
+
+
+#  \n
+print("Monday", "Tuesday", "Wednesday", sep="\n")
+
+
+#  
+ #print("Hello", "World!", end=" ")
+print("Hello", end=" ")
+print("World!")
