c3639682c177c7c9f23d3d0d2b9efe6466c6280a
Sudeep Devkota <devkotasudeep615@gmail.com>
2026-05-21 16:43:06 +0545

    First Question Uploaded

---
+++ Lesson3_Homework/hw_1.py
@@ -0,0 +1,9 @@
+type_1 = 7
+type_2 = True
+type_3 = "Hello, IHL!!!"
+type_4 = 3.14
+
+print("The type of this literal is: ", type(type_1))
+print("The type of this literal is: ", type(type_2))
+print("The type of this literal is: ", type(type_3))
+print("The type of this literal is: ", type(type_4))
