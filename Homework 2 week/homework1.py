80549e0eb70b3fb75e44464e43596a80396bfa4e
crazylad-ctrl <ujjwalsubedi603@gmail.com>
2026-05-26 19:39:01 +0545

    Add Homework 2 week assignments

---
+++ Homework 2 week/homework1.py
@@ -0,0 +1,14 @@
+type_1 = 12
+type_2 = True
+type_3 = "I am Happy to be in IHL!!!"
+type_4 = 2.134
+
+print("The type of this literal is: ", type(type_1)) 
+ #prints type of lateral 
+print("The type of this literal is: ", type(type_2)) 
+#prints type of lateral True
+print("The type of this literal is: ", type(type_3))
+ #prints type of lateral "I am Happy to be in IHL!!!"
+print("The type of this literal is: ", type(type_4)) 
+#printts type of lateral 2.134
+#Finished
