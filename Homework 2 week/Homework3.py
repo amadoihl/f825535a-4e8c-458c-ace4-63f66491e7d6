0c99d748945782038e5517c5439f7ebab3c4ec25
crazylad-ctrl <ujjwalsubedi603@gmail.com>
2026-05-26 19:43:49 +0545

    Revert "Remove deleted homework files"
    This reverts commit cf39d2c29dc6ef4407b232ca977e7796e2adf667.

---
+++ Homework 2 week/Homework3.py
@@ -0,0 +1,35 @@
+positive_integer = 55          
+#Positive integer
+
+negative_integer = -12     
+#  Negative integer
+
+positive_float = 3.14       
+#  Positive float
+
+negative_float = -98.76     
+ #  Negative floatc
+
+negative_float_scientific = -2.5e4
+#  Negative float using scientific notation
+
+print("Positive integer",positive_integer)     
+ # prints the positive integer value
+
+
+print("Negative integer",negative_integer)          
+# prints the negative integer value
+
+
+print("Positive float",positive_float)             
+# prints the positive float value
+
+
+print("Negative float",negative_float)            
+# prints the negative float value
+
+
+print("Negative float in scientific notation",negative_float_scientific)  
+# prints the negative float in scientific notation
+
+#Finished
