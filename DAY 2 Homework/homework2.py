e3a55866e62a379702ca7878534f330159f03327
crazylad-ctrl <ujjwalsubedi603@gmail.com>
2026-05-26 19:45:39 +0545

    Move homework files from 'Homework 2 week' to 'DAY 2 Homework' folder

---
+++ DAY 2 Homework/homework2.py
@@ -0,0 +1,20 @@
+is_alive = True        # bool variable
+age = 12                # int variable
+name = "Ujjwal"          # str variable
+weight = 19.6      # float variable
+
+print("The type of this variable is:", type(is_alive))
+# prints the type of bool variable
+
+
+print("The type of this variable is:", type(age))         
+# prints the type of int variable
+
+
+print("The type of this variable is:", type(name))  
+ # prints the type of str variable
+
+ 
+print("The type of this variable is:", type(weight)) 
+# prints the type of float variable
+#Finished
