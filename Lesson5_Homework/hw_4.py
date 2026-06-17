68f7750d6e70ac874579eef4e683104fa613c286
Sudeep Devkota <devkotasudeep615@gmail.com>
2026-06-17 15:43:18 +0545

    Homework 5

---
+++ Lesson5_Homework/hw_4.py
@@ -0,0 +1,18 @@
+a = 51
+b = 17
+
+#arthematic operations
+print("Addition:", a, "+", b, "=", a + b)
+print("Subtraction:", a, "-", b, "=", a - b)
+print("Multiplication:", a, "*", b, "=", a * b)
+print("Division:", a, "/", b, "=", a / b)
+print("Floor Division:", a, "//", b, "=", a // b)
+print("Modulo (Remainder):", a, "%", b, "=", a % b)
+print("Exponentiation:", a, "**", b, "=", a ** b)
+
+#bitwise operations
+print("Bitwise AND:", a, "&", b, "=", a & b)
+print("Bitwise OR:", a, "|", b, "=", a | b)
+print("Bitwise XOR:", a, "^", b, "=", a ^ b)
+print("Bitwise Left Shift:", a, "<<", b, "=", a << b)
+print("Bitwise Right Shift:", a, ">>", b, "=", a >> b)
